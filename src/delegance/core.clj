0f9feb55d43e3858c9f71797116955a2ae2b6594
James Reeves <jreeves@weavejester.com>
2012-12-13 21:55:34 +0000

    Blocking result using a delay

--- src/delegance/core.clj
+++ src/delegance/core.clj
@@ -2,19 +2,18 @@
   (:require [delegance.protocols :refer :all]
             [delegance.util :refer (random-uuid)]))
 
-(deftype DelegateResult [client job-id]
-  clojure.lang.IDeref
-  (deref [_]
-    (-> (:state client)
-        (get! job-id)
-        (:result))))
+(defn- poll-while-nil [rate func]
+  (loop []
+    (or (func)
+        (do (Thread/sleep rate)
+            (recur)))))
 
 (defn delegate-eval [client form]
   (let [job-id (random-uuid)
         {:keys [queue state store]} client]
     (put state job-id {:form form})
     (push queue job-id)
-    (DelegateResult. client job-id)))
+    (delay (poll-while-nil 1000 #(:result (get! state job-id))))))
 
 (defmacro delegate [client form]
   `(delegate-eval ~client '~form))
