43285804eb4249dc40ae33e9a65fda905ae51c25
James Reeves <jreeves@weavejester.com>
2012-12-13 20:49:17 +0000

    Non-blocking results from delegate

--- src/delegance/core.clj
+++ src/delegance/core.clj
@@ -1,8 +1,20 @@
 (ns delegance.core
-  (:require [delegance.protocols :refer :all]))
+  (:require [delegance.protocols :refer :all]
+            [delegance.util :refer (random-uuid)]))
+
+(deftype DelegateResult [client job-id]
+  clojure.lang.IDeref
+  (deref [_]
+    (-> (:state client)
+        (get! job-id)
+        (:result))))
 
 (defn delegate-eval [client form]
-  (push (:queue client) form))
+  (let [job-id (random-uuid)
+        {:keys [queue state store]} client]
+    (put state job-id {:form form})
+    (push queue job-id)
+    (DelegateResult. client job-id)))
 
 (defmacro delegate [client form]
   `(delegate-eval ~client '~form))
--- src/delegance/worker.clj
+++ src/delegance/worker.clj
@@ -9,9 +9,11 @@
   (.scheduleAtFixedRate executor runnable 0 rate TimeUnit/SECONDS))
 
 (defn- process-available-jobs [{:keys [queue state store]}]
-  (when-let [job (reserve queue 300)]
+  (when-let [{job-id :data :as job} (reserve queue 300)]
     (try
-      (eval (:data job))
+      (let [job-state (get! state job-id)
+            result    (eval (:form job-state))]
+        (modify state job-id assoc :result result))
       (finally (finish queue job)))))
 
 (defn worker
