3b8aa4589e0219754002cd7b242021d54911e436
James Reeves <jreeves@weavejester.com>
2013-05-21 17:24:21 +0100

    Replaced delay with RemotePromise type

--- src/delegance/core.clj
+++ src/delegance/core.clj
@@ -2,19 +2,21 @@
   (:require [delegance.protocols :refer :all]
             [delegance.util :refer (random-uuid)]))
 
-(defn- poll-while-nil [rate func]
-  (loop []
-    (or (func)
-        (do (Thread/sleep rate)
-            (recur)))))
+(deftype RemotePromise [getter poll-rate no-data]
+  clojure.lang.IDeref
+  (deref [rp]
+    (loop []
+      (let [value (getter)]
+        (if (= value no-data)
+          (do (Thread/sleep poll-rate) (recur))
+          value)))))
 
 (defn delegate-eval [client form]
   (let [{queue :queue, state :state} client
         job-id  (random-uuid)]
-    (put state job-id {:form form})
+    (put state job-id {:form form :result ::no-data})
     (push queue job-id)
-    (delay
-     (poll-while-nil 1000 #(:result (get! state job-id))))))
+    (RemotePromise. #(:result (get! state job-id)) 1000 ::no-data)))
 
 (defn resolve-form [form]
   (eval (read-string (str \` (pr-str form)))))
