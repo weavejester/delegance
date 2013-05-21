0d7fba43fb36e7a00e7eb54766fe781391d59c11
James Reeves <jreeves@weavejester.com>
2013-05-21 17:53:23 +0100

    Added some additional tests for timeouts and nils

--- test/delegance/core_test.clj
+++ test/delegance/core_test.clj
@@ -10,5 +10,18 @@
 
 (deftest test-delegate
   (run-worker client)
-  (let [x (delegate client (+ 1 1))]
-    (is (= @x 2))))
+  (testing "simple deref"
+    (let [x (delegate client (+ 1 1))]
+      (is (= @x 2))))
+  (testing "nil result"
+    (let [x (delegate client nil)]
+      (is (nil? (deref x 5000 :fail)))))
+  (testing "timeout values"
+    (let [x (delegate client (Thread/sleep 3000))]
+      (is (= (deref x 1000 :timeout) :timeout))))
+  (testing "timeout times"
+    (let [x  (delegate client (Thread/sleep 3000))
+          t1 (System/currentTimeMillis)]
+      (deref x 1500 :timeout)
+      (let [t2 (System/currentTimeMillis)]
+        (is (<= 1400 (- t2 t1) 1600))))))
