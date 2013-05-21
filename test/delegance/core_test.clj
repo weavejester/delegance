0709f95594af3e8e57bf4a8b02d39fa11f76cd5f
James Reeves <jreeves@weavejester.com>
2013-05-21 17:55:30 +0100

    Ensure tests have enough workers

--- test/delegance/core_test.clj
+++ test/delegance/core_test.clj
@@ -9,13 +9,11 @@
    :state (memory-state)})
 
 (deftest test-delegate
-  (run-worker client)
+  (dotimes [_ 10]
+    (run-worker client))
   (testing "simple deref"
     (let [x (delegate client (+ 1 1))]
       (is (= @x 2))))
-  (testing "nil result"
-    (let [x (delegate client nil)]
-      (is (nil? (deref x 5000 :fail)))))
   (testing "timeout values"
     (let [x (delegate client (Thread/sleep 3000))]
       (is (= (deref x 1000 :timeout) :timeout))))
@@ -24,4 +22,7 @@
           t1 (System/currentTimeMillis)]
       (deref x 1500 :timeout)
       (let [t2 (System/currentTimeMillis)]
-        (is (<= 1400 (- t2 t1) 1600))))))
+        (is (<= 1400 (- t2 t1) 1600)))))
+  (testing "nil result"
+    (let [x (delegate client nil)]
+      (is (nil? (deref x 3000 :fail))))))
