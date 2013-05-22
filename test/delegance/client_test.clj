cca936dd07176373cb9694fea66e98bf78bb80ac
James Reeves <jreeves@weavejester.com>
2013-05-22 15:45:17 +0100

    Renamed client to config in client-test namespace

--- test/delegance/client_test.clj
+++ test/delegance/client_test.clj
@@ -4,25 +4,25 @@
         delegance.memory
         delegance.worker))
 
-(def client
+(def config
   {:queue (memory-queue)
    :store (memory-store)})
 
 (deftest test-delegate
   (dotimes [_ 10]
-    (run-worker client))
+    (run-worker config))
   (testing "simple deref"
-    (let [x (delegate client `(+ 1 1))]
+    (let [x (delegate config `(+ 1 1))]
       (is (= @x 2))))
   (testing "timeout values"
-    (let [x (delegate client `(Thread/sleep 3000))]
+    (let [x (delegate config `(Thread/sleep 3000))]
       (is (= (deref x 1000 :timeout) :timeout))))
   (testing "timeout times"
-    (let [x  (delegate client `(Thread/sleep 3000))
+    (let [x  (delegate config `(Thread/sleep 3000))
           t1 (System/currentTimeMillis)]
       (deref x 1500 :timeout)
       (let [t2 (System/currentTimeMillis)]
         (is (<= 1400 (- t2 t1) 1600)))))
   (testing "nil result"
-    (let [x (delegate client nil)]
+    (let [x (delegate config nil)]
       (is (nil? (deref x 3000 :fail))))))
