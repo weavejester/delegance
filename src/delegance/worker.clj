9b7d30a7a2c31b45fc1dc6f5d52593fb66debfa1
James Reeves <jreeves@weavejester.com>
2013-05-28 15:57:22 +0100

    Added custom :eval functions for workers

--- src/delegance/worker.clj
+++ src/delegance/worker.clj
@@ -4,7 +4,9 @@
   (:import [java.util.concurrent ScheduledThreadPoolExecutor TimeUnit])
   (:require [delegance.protocols :refer :all]))
 
-(defn- process-available-jobs [{queue :queue, store :store}]
+(defn- process-next-job
+  "Process the next job on the job queue."
+  [{:keys [queue store eval] :or {eval clojure.core/eval}}]
   (when-let [[job-id store-id] (reserve queue)]
     (try
       (let [job-store (get! store store-id)
@@ -17,18 +19,16 @@
         (finish queue job-id)))))
 
 (defn run-worker
-  "Start a worker process running that will poll a queue at a specified rate
-  in milliseconds. The config should contain a :queue and a :store, which
-  should respectively implement the Queue and Store protocols in the
+  "Start a worker process running that will periodically poll a queue for work.
+  The config should contain at least a :queue and a :store, which should
+  respectively implement the Queue and Store protocols in the
   delegance.protocols namespace. Items should be pushed onto the queue using
   the delegance.core/delegate function. This function returns a worker map."
-  ([config]
-     (run-worker config 1000))
-  ([config rate]
-     (let [executor (ScheduledThreadPoolExecutor. 1)
-           process  #(process-available-jobs config)]
-       (.scheduleAtFixedRate executor process 0 rate TimeUnit/MILLISECONDS)
-       {:client config :executor executor})))
+  [{:keys [rate] :or {rate 1000} :as config}]
+  (let [executor (ScheduledThreadPoolExecutor. 1)
+        process  #(process-next-job config)]
+    (.scheduleAtFixedRate executor process 0 rate TimeUnit/MILLISECONDS)
+    {:config config :executor executor}))
 
 (defn shutdown-worker
   "Shut down a worker started with the run-worker function."
